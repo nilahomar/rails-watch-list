f6a2024ea93dfd4ce98324170b3f6ef34f94cbd4
Nila Homar <nilahomar@gmail.com>
2023-02-24 16:02:23 +0100

    movie lists and bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+before_action :set_list, only: [:new, :create]
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    # @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    # @movie = Movie.find(params[:bookmark] [:movie_id])
+    # @bookmark.movie = @movie
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+
+  def destroy
+    bookmark = Bookmark.find(params[:id])
+    bookmark.destroy
+    redirect_to list_path(bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
